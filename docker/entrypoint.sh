4a3a1ae87ef76e7a3a47f7a162d5d8f9008120f9
Darío Rivera <fermius.us@gmail.com>
2020-02-07 08:36:30 -0500

    Set up initial laravel environment with app and db

---
+++ docker/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#RUN chown -R www-data:www-data /var/www
+#RUN chmod -R a+w /var/www/jobsitychallenge/dariorivera/storage
+#RUN chmod a+w /var/www/jobsitychallenge/dariorivera/bootstrap/cache
+
+echo "All services are running"
